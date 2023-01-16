f540437e5fe96296f1e59166b3e21b16e8884cd2
Khánh Nguyễn <116017175+Charlottei3@users.noreply.github.com>
2023-01-16 13:45:48 +0700

    update lv9

--- Assets/scripts/lv8/ver2ControlLv8.cs
+++ Assets/scripts/lv8/ver2ControlLv8.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -16,11 +17,13 @@ public class ver2ControlLv8: MonoBehaviour
     [SerializeField] List<Image> _instanceObj;
     [SerializeField] List<Image> _boxSelected;
 
-   
+    int idSelect;
+
 
     private void Start()
     {
-        
+     
+        ranItem();
     }
 
     public void ranItem()
@@ -32,16 +35,51 @@ public class ver2ControlLv8: MonoBehaviour
         }
 
         int _ran_1 = Random.Range(0, _addSprite.Count);
+        _instanceObj[0].sprite = _addSprite[_ran_1];
+        _instanceObj[3].sprite = _addSprite[_ran_1];
         _addSprite.RemoveAt(_ran_1);
-        int _ran_2 = Random.Range(0, _addSprite.Count);
-        _addSprite.RemoveAt(_ran_2);
-        int _ran_3 = Random.Range(0, _addSprite.Count);
 
+        int _ran_2 = Random.Range(0, _addSprite.Count);
+        _instanceObj[1].sprite = _addSprite[_ran_2];
+        _instanceObj[4].sprite = _addSprite[_ran_2];
+        _addSprite.RemoveAt(_ran_2);
+
+        int _ran_3 = Random.Range(0, _addSprite.Count);
+        _instanceObj[2].sprite = _addSprite[_ran_3];
+        _instanceObj[5].sprite = _addSprite[_ran_3];
+
+
+        int _hoiCham = Random.Range(0, _instanceObj.Count - 1);
+
+        int _ranTrue = Random.Range(0, 1);
+        _ranTrue = idSelect;
+
+        _boxSelected[_ranTrue].sprite = _instanceObj[_hoiCham].sprite;
+        Debug.Log("id bx1 :  " + _boxSelected[_ranTrue].name);
+        Debug.Log("id bx1 image true:  " + _instanceObj[_hoiCham].sprite.name);
+
+        _gameObjs.Remove(_instanceObj[_hoiCham].sprite);
+        int _ran = Random.Range(1, _gameObjs.Count - 1);
         
-        for (int i = 0; i < _instanceObj.Count; i++)
-        {
-            
-        }
+        _boxSelected[_ranTrue == 0 ? 1 : 0].sprite = _instanceObj[_ran].sprite;
+
+      
+        Debug.Log("id bx2:  " + _boxSelected[_ranTrue == 0 ? 1 : 0]);
+        Debug.Log("name image false: " + _instanceObj[_ran].sprite.name);
+
+        _instanceObj[_hoiCham].sprite = _gameObjs[0];
+
     }
-   
+
+    public void selectedItem(int id)
+    {
+        if (id == idSelect)
+        {
+            _instanceObj[idSelect].sprite = _instanceObj[idSelect].sprite;
+        }
+
+        Debug.Log(idSelect);
+        Debug.Log(id);
+    }
+
 }
